64374c34b054fe6dcae7df817e9cb9264189db35
PROFESSOR K10 <professork10@acad.unifor.br>
2025-03-31 12:26:39 -0300

    Pesquisa Binária Iterativa e Recursiva

--- src/App.java
+++ src/App.java
@@ -5,13 +5,15 @@ public class App {
         int valores[] = new int[]{7, 1, 2, 5, 3};
         int alvo = 2;
 
-        int posicao = pesquisaLinearRecursiva(valores, alvo);
-        System.out.println("Na posicao "+posicao);
-
+        // int posicao = pesquisaLinearRecursiva(valores, alvo);
+        // System.out.println("Na posicao "+posicao);
         
-
+        Arrays.sort(valores);
+        int posicao = pesquisaBinariaRecursiva(valores, alvo, 0, valores.length - 1);
+        System.out.println("Na posicao "+posicao);
+        
     }
-    
+        
     /*
      * T(n) = n + 1 + n = 2n + 1 = O(n)
      */
@@ -37,6 +39,31 @@ public class App {
         return pesquisaLinearRecursiva(valores, alvo, atual + 1);
     }
 
+    private static int pesquisaBinariaIterativa(int[] valores, int alvo, int inicio, int fim) {
+        while(inicio <= fim) {
+            int meio = (fim + inicio)/2;
 
+            if (valores[meio] == alvo) return meio;
+
+            if (valores[meio] > alvo) fim = meio - 1;
+            else inicio = meio + 1;
+        }
+
+        return -1;
+    }
+
+    /*
+     * T(n) = 
+     */
+    private static int pesquisaBinariaRecursiva(int[] valores, int alvo, int inicio, int fim) {
+        if(inicio > fim) return -1;
+
+        int meio = (fim + inicio)/2;
+
+        if (valores[meio] == alvo) return meio;
+        
+        if (valores[meio] > alvo) return pesquisaBinariaRecursiva(valores, alvo, inicio, meio - 1);
+        else return pesquisaBinariaRecursiva(valores, alvo, meio + 1, fim);
+    }
 
 }
