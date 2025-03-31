8a96c8f247ceb3bac55bb4a46a382cc55b601770
PROFESSOR K10 <professork10@acad.unifor.br>
2025-03-31 12:06:26 -0300

    Análise Pesquisa Linear

--- src/App.java
+++ src/App.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class App {
     public static void main(String[] args) throws Exception {
         int valores[] = new int[]{7, 1, 2, 5, 3};
@@ -5,14 +7,17 @@ public class App {
 
         int posicao = pesquisaLinearRecursiva(valores, alvo);
         System.out.println("Na posicao "+posicao);
+
+        
+
     }
     
     /*
-     * 
+     * T(n) = n + 1 + n = 2n + 1 = O(n)
      */
     private static int pesquisaLinearIterativa(int[] valores, int alvo) {
-        for (int pos = 0; pos < valores.length; pos++)
-            if (valores[pos] == alvo) return pos;
+        for (int pos = 0; pos < valores.length; pos++)//n + 1
+            if (valores[pos] == alvo) return pos;//n
 
         return -1;
     }
@@ -22,7 +27,7 @@ public class App {
     }
     
     /*
-     * 
+     * T(n) = T(n - 1) + c = cn = O(n)
      */
     private static int pesquisaLinearRecursiva(int[] valores, int alvo, int atual) {
         if (atual >= valores.length) return -1;
